524925c4b96ccdc443ec88dab98b69ad6c310edf
songshengping <546636494@qq.com>
2020-05-19 09:59:30 +0800

    阿里开发手册 - 编程规约 - (十)其他

--- P7-platform-common/src/main/java/com/song/p7/platform/utils/ValidateUtils.java
+++ P7-platform-common/src/main/java/com/song/p7/platform/utils/ValidateUtils.java
@@ -10,21 +10,23 @@ import java.util.regex.Pattern;
  * @Created by Jeremy
  */
 public class ValidateUtils {
+    /**
+     * 阿里开发手册 - 编程规约 - (十)其他
+     * 1. 【强制】在使用正则表达式时，利用好其预编译功能，可以有效加快正则匹配速度。
+     */
+    public static Pattern emailPatten = Pattern.compile("^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$");
+    public static Pattern phonePatten = Pattern.compile("^[1]\\d{10}$");
     public static boolean isEmail(String email) {
         if (StringUtils.isBlank(email)) {
             return false;
         }
-        String regex = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
-        Pattern pattern = Pattern.compile(regex);
-        return pattern.matcher(email).matches();
+        return emailPatten.matcher(email).matches();
     }
 
     public static boolean isPhoneNumber(String phoneNumber){
         if (StringUtils.isBlank(phoneNumber)) {
             return false;
         }
-        String regex = "^[1]\\d{10}$";
-        Pattern pattern = Pattern.compile(regex);
-        return pattern.matcher(phoneNumber).matches();
+        return phonePatten.matcher(phoneNumber).matches();
     }
 }
